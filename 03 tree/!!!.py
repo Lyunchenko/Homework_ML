8e4cbb454117d0e4695590f90fca5fed4734d268
Lyunchenko <Stanislav.Lyunchenko@chtpz.ru>
2018-10-17 16:44:39 +0500

    Tree

--- 03 tree/!!!.py
+++ 03 tree/!!!.py
@@ -1,42 +1,79 @@
 import numpy as np
 
-a = [10, 11, 12]
-b = np.array([[1, 2, 3], [0, 5, 6], [7, 8, 9]])
+'''
+	a = [10, 11, 12]
+	b = np.array([[1, 2, 3], [0, 5, 6], [7, 8, 9]])
 
-print(b)
+	print(b)
 
-b = np.append(b, [a], axis=0)
+	b = np.append(b, [a], axis=0)
 
-a = [10, 11, 12, 13]
-b = np.c_[b, a]
-b = np.c_[b, a]
+	a = [10, 11, 12, 13]
+	b = np.c_[b, a]
+	b = np.c_[b, a]
 
-print(b)
+	print(b)
 
-print('\nShape')
-q = np.array([1, 2, 2, 2, 1, 1, 3])
-print(q.shape[0])
+	print('\nShape')
+	q = np.array([1, 2, 2, 2, 1, 1, 3])
+	print(q.shape[0])
 
-print(b[2:])
+	print(b[2:])
 
-print('\nSotr')
-print(b[b[:,0].argsort()])
+	print('\nSotr')
+	print(b[b[:,0].argsort()])
 
 
-print('\nUnic value')
-q = np.array([1, 2, 2, 2, 1, 1, 3])
+	print('\nUnic value')
+	q = np.array([1, 2, 2, 2, 1, 1, 3])
+	print(q)
+	unique_rows = np.unique(q, axis=0)
+
+	print(unique_rows)
+
+
+	print('\nValue count')
+	q = np.array([0, 2, 2, 2, 1, 1, 3])
+	unique_val, val_count = np.unique(q, return_counts=True, axis=0)
+	print(unique_val, val_count)
+	arr = np.c_[unique_val, val_count]
+	print(arr)
+
+	dtype = dict.fromkeys([1, 2], (10, 20))
+	print(dtype)
+'''
+
+'''
+q = np.array([1, 2, 2, 2, 1, 1, 3, 4, 4, 3])
 print(q)
-unique_rows = np.unique(q, axis=0)
+q = np.sort(q)[:-1]
+print(q)
 
-print(unique_rows)
+q = q[q>1]
+print(q)
+'''
 
+'''
+b = np.array([[2, 2, 3], [1, 5, 6], [7, 8, 9]])
+print(b)
+print('\n')
+print(b[b[:,0]>1])
+'''
 
-print('\nValue count')
-q = np.array([0, 2, 2, 2, 1, 1, 3])
-unique_val, val_count = np.unique(q, return_counts=True, axis=0)
-print(unique_val, val_count)
-arr = np.c_[unique_val, val_count]
-print(arr)
+'''
+q = {'q':1, 'w': 2}
+w = [{'e':1, 'r': 2}, {'q':1, 'w': 2}]
 
-dtype = dict.fromkeys([1, 2], (10, 20))
-print(dtype)
+q = [q]
+q.extend([w])
+print(q)
+'''
+
+b = np.array([[1, 2, 3], [0, 5, 6], [7, 8, 9]])
+a = [10, 11, 12]
+b = np.c_[b, a]
+
+print(b)
+b[:,0] = b[:,0]*2
+print(b)
+
--- 03 tree/tree.py
+++ 03 tree/tree.py
@@ -16,7 +16,7 @@ class DecisionTree(BaseEstimator):
 
     def fit(self, X, y):
         data = np.c_[X, y]
-        self.instruction = self._best_split(data)
+        self.instruction = self._get_instruction(data, '1')
     
 
         
@@ -28,13 +28,12 @@ class DecisionTree(BaseEstimator):
         pass
 
     def _entropy(self, y):
-        val, count = np.unique(y, return_counts=True, axis=0)
-        value_count = np.c_[val, count]
-        count_y = y.shape[0]
+        proba = self._get_proba(y)
         entropy = 0
-        for x in value_count:
-            p = x[1]/count_y
-            entropy -= p*mt.log(p, 2)
+        print(proba)
+        for x in proba:
+            
+            entropy -= x[1]*mt.log(x[1], 2)
         return(entropy)
 
     def _gini(self, y):
@@ -46,11 +45,73 @@ class DecisionTree(BaseEstimator):
     def _mad_median(self, y):
         pass
 
+    def _get_proba(self, y):
+        val, count = np.unique(y, return_counts=True, axis=0)
+        value_count = np.c_[val, count]
+        count_y = y.shape[0]
+        print(value_count)
+        proba = np._c[value_count[:,0],value_count[:,1]/count_y]
+        print(proba)
+        return(proba)
+        
+
+    def _information_gain(self, data, data_left, data_right):
+        count = data.shape[0]
+        count_left = data_left.shape[0]
+        count_right = data_right.shape[0]
+        fval = self.criterion_func(data)
+        fval_left = self.criterion_func(data_left)
+        fval_right = self.criterion_func(data_right)
+        ig = fval - fval_left*count_left/count - fval_right*count_right/count
+        return(ig)
 
     def _best_split(self, data):
         index_y = data.shape[1]
         count_x = index_y-1
+        best_split = {'ig': 0, 'column': 0, 'val': 0}
         for i in range(count_x):
-            col = data[:, i]
-            unique_val = np.unique(col, axis=0)
-            
+            unique_val = np.unique(data[:, i], axis=0)
+            unique_val = np.sort(unique_val)[-1]
+            for val in unique_val:
+                data_left = data[data[:,i]<=val]
+                data_right = data[data[:,i]>val]
+                ig = self._information_gain(data[:, index_y], data_left[:, index_y], data_right[:, index_y])
+                if ig > best_split['ig']:
+                    best_split['ig'] = ig
+                    best_split['column'] = i
+                    best_split['val'] = val
+        if best_split['ig'] == 0:
+            return(False)
+        else:
+            return(best_split)
+
+    def _get_instruction(self, data, id_instruction):
+        split = self._best_split(data)
+        if split:
+            # Деление ветвей
+            id_instr_left = id_instruction + '1'
+            id_instr_right = id_instruction + '2'
+            instruction = [{'id': id_instruction,
+                           'column': split['column'], 
+                           'val': split['val'],
+                           'id_left': id_instr_left,
+                           'id_right': id_instr_right,
+                           'leaf_data': False}]
+            data_left = data[data[:,split['column']]<=split['val']]
+            data_right = data[data[:,split['column']]>split['val']]
+            instruction.extend(self._get_instruction(data_left, id_instr_left))
+            instruction.extend(self._get_instruction(data_right, id_instr_right))
+            return(instruction)
+        else:
+            # Формирование листа дерева
+
+            instruction = [{'id': id_instruction,
+                           'column': None, 
+                           'val': None,
+                           'id_left': None,
+                           'id_right': None,
+                           'leaf_data': False}]
+            pass
+    
+
+        
